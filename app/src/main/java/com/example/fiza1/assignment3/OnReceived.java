b678b901e14592bba89bb871888a22d113e6ff0a
Fiza1 <fizahabib895@gmail.com>
2017-12-25 18:11:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fiza1/assignment3/OnReceived.java
@@ -0,0 +1,14 @@
+package com.example.fiza1.assignment3;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class OnReceived {
+    Context context;
+    Intent intent;
+
+    public OnReceived(Context context, Intent intent) {
+        this.context = context;
+        this.intent = intent;
+    }
+}
